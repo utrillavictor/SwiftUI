af789987e79adddeb335506b72b7124ae8cba004
utrillavictor <utrilla.victor@gmail.com>
2020-04-16 22:30:45 -0700

    Completed Day 14

--- Day 12/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Day 12/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -11,6 +11,8 @@ import SwiftUI
 struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    @State private var score = 0
+    @State private var message = ""
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     
@@ -32,7 +34,6 @@ struct ContentView: View {
                 ForEach(0 ..< 3) { number in
                     Button(action: {
                         self.flagTapped(number)
-                        self.showingScore = true
                     }) {
                         Image(self.countries[number])
                             .renderingMode(.original)
@@ -42,23 +43,29 @@ struct ContentView: View {
                     }
                 }
                 
+                Text("Your score is \(score)")
+                    .foregroundColor(.white)
+                    .font(.subheadline)
+                    .fontWeight(.bold)
+                
                 Spacer()
             }
         }
         .alert(isPresented: $showingScore) {
-            Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+            Alert(title: Text(scoreTitle), message: Text(message), dismissButton: .default(Text("Continue")) {
                 self.askQuestion()
                 })
         }
     }
     
     func flagTapped(_ number: Int) {
+        scoreTitle = number == correctAnswer ? "Correct" : "Wrong"
         if number == correctAnswer {
-            scoreTitle = "Correct"
+            score += 1
         } else {
-            scoreTitle = "Wrong"
+            score = score <= 0 ? 0 : score - 1
         }
-        
+        message = number == correctAnswer ? "" : "That's the flag of \(countries[number])"
         showingScore = true
     }
     
--- Day 12/README.md
+++ Day 12/README.md
@@ -1,5 +1,8 @@
 # GuessTheFlag App
 <p align="center">
+    <img src="Screenshot.png" width="456" max-width="90%" alt="Guess the flag" />
+</p>
+<p align="center">
     <img src="https://img.shields.io/badge/Swift-5.2-orange.svg" />
     <img src="https://img.shields.io/badge/xcode-11.4-brightgreen.svg" />
     <img src="https://img.shields.io/badge/platforms-mac+linux-brightgreen.svg?style=flat" alt="Mac + Linux" />
@@ -17,3 +20,7 @@ Guess The Flag SwiftUI Tutorial by Paul Hudson
 * Stacking up buttons
 * Showing alert
 * Styling flags
+
+* Wrapp Up
+* Show score
+* Dynamic message
