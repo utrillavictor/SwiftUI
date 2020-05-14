bb65798e9d66bbff0086bfe6d5948d1bb2045613
utrillavictor <utrilla.victor@gmail.com>
2020-05-13 22:08:29 -0700

    Project 9 Part 3

--- Day 33/Drawing/Drawing/ContentView.swift
+++ Day 33/Drawing/Drawing/ContentView.swift
@@ -8,6 +8,62 @@
 
 import SwiftUI
 
+struct Trapezoid: Shape {
+    var insetAmount: CGFloat
+    var animatableData: CGFloat {
+        get { insetAmount }
+        set { self.insetAmount = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+
+        return path
+   }
+}
+
+struct TrapezoidContentView: View {
+    @State private var insetAmount: CGFloat = 50
+
+    var body: some View {
+        Trapezoid(insetAmount: insetAmount)
+            .frame(width: 200, height: 100)
+            .onTapGesture {
+                withAnimation {
+                    self.insetAmount = CGFloat.random(in: 10...90)
+                }
+            }
+    }
+}
+
+struct BlendModesContentView: View {
+    @State private var amount: CGFloat = 0.0
+
+    var body: some View {
+        VStack {
+            ZStack {
+                Circle()
+                    .fill(Color.blue)
+                    .frame(width: 200 * amount)
+                    .offset(x: -50, y: -80)
+                    .blendMode(.screen)
+            }
+            .frame(width: 300, height: 300)
+
+            Slider(value: $amount)
+                .padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
 struct Arc: InsettableShape {
     var startAngle: Angle
     var endAngle: Angle
@@ -73,6 +129,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        TrapezoidContentView()
     }
 }
